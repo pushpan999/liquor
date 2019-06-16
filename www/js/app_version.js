fd65bc4ef946a33400f44d659e23b93ac310e7e7
Pushpan Kumar GC <getpushpan@gmail.com>
2019-06-16 15:14:24 +0545

    added auto update

---
+++ www/js/app_version.js
@@ -0,0 +1,96 @@
+var hasLatestUpdate = function(successCallback) {
+    var versionOptions = {
+        url: "http://liquor.pushpan.com.np/app_version.json",
+        dataType: "JSON",
+        requestType: 'GET',
+        callBack: function(response) {
+            var apkVersion = response.data.version;
+            cordova.getAppVersion(function(version) {
+                if (apkVersion != version) {
+                    successCallback(false, apkVersion);
+                } else {
+                    successCallback(true, apkVersion);
+                }
+            });
+        }
+    };
+    CommunityApp.dataAccess.callService(versionOptions);
+};
+var forceUpdate = function(callBack) {
+    hasLatestUpdate(function(isUpdated, newVersion) {
+        if (newVersion !== null) {
+            var updateInProgress = CommunityApp.session.load("update-inprogress", true);
+            if (updateInProgress != newVersion) {
+                if (!isUpdated) {
+                    CommunityApp.session.save("update-inprogress", newVersion, true);
+                    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+                        var apkFile = 'download/';
+                        var permissions = cordova.plugins.permissions;
+                        permissions.hasPermission(permissions.WRITE_EXTERNAL_STORAGE, function(status) {
+                            if (!status.hasPermission) {
+                                var errorCallback = function() {
+                                    alert("Error: app requires storage permission");
+                                    if (callBack && callBack !== null) {
+                                        callBack();
+                                    }
+                                };
+                                permissions.requestPermission(permissions.WRITE_EXTERNAL_STORAGE,
+                                    function(status) {
+                                        if (!status.hasPermission)
+                                            errorCallback();
+                                        else {
+                                            downloadFile(fileSystem);
+                                        }
+                                    },
+                                    errorCallback);
+                            } else {
+                                downloadFile(fileSystem);
+                            }
+                        }, null);
+                        var downloadFile = function(fileSystem) {
+                            var localPath = fileSystem.root.toURL() + 'download/new-android.apk',
+                                fileTransfer = new FileTransfer();
+                            fileTransfer.download(CommunityApp.configuration.appConfig.apkUrl, localPath, function(entry) {
+                                window.plugins.webintent.startActivity({
+                                        action: window.plugins.webintent.ACTION_VIEW,
+                                        url: localPath,
+                                        type: 'application/vnd.android.package-archive'
+                                    },
+                                    function() {},
+                                    function(e) {
+                                        alert("Failed to update the app!");
+                                        if (callBack && callBack !== null) {
+                                            callBack();
+                                        }
+                                    }
+                                );
+                            }, function(error) {
+                                alert("Error downloading the latest updates! - error: " + JSON.stringify(error));
+                                if (callBack && callBack !== null) {
+                                    callBack();
+                                }
+                            });
+                        };
+                    }, function(evt) {
+                        alert("Error preparing to download the latest updates! - Err - " + evt.target.error.code);
+                        if (callBack && callBack !== null) {
+                            callBack();
+                        }
+                    });
+                } else {
+                    if (callBack && callBack !== null) {
+                        callBack();
+                    }
+                }
+            } else {
+                if (callBack && callBack !== null) {
+                    callBack();
+                }
+            }
+        } else {
+            if (callBack && callBack !== null) {
+                callBack();
+            }
+        }
+    });
+};
