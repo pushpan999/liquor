4cbb2b52b42c1689019f0de7a4be7b7442d919fd
Pushpan Kumar G C <pushpan@Pushpans-MacBook-Pro.local>
2020-09-02 17:10:16 +0545

    api added

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
     $('#rate-lists').DataTable( {  
         "ordering": true,           
         "pageLength": 50,
-        "ajax": "data.json"
+        "ajax": "https://liquor.pushpan.com.np/api/items/?format=datatables",
+	"columns": [
+            { "data": "title" },
+            { "data": "category" },
+            { "data": "vol" },
+            { "data": "mrp" },
+            { "data": "ws_aft_vat" },
+            { "data": "retail_bf_vat" },
+	    { "data": "retail_aft_vat" },
+
+        ]
     } );
 } );     
 
