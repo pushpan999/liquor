cc02df02eaa8b4ab3d7ee600b79046fda354c84b
Pushpan Kumar G C <pushpan@Pushpans-MacBook-Pro.local>
2020-09-06 17:15:41 +0545

    pulltorefresh.js added

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,25 @@ $(document).ready(function() {
 
         ]
     } );
-} );     
+} );    
+
+$(document).ready(function() {
+    $('#old-rate-lists').DataTable( {  
+        "ordering": true,           
+        "pageLength": 50,
+        "ajax": "data.json",
+	"columns": [
+            { "data": "title" },
+            { "data": "category" },
+            { "data": "vol" },
+            { "data": "mrp" },
+            { "data": "ws_aft_vat" },
+            { "data": "retail_bf_vat" },
+	    { "data": "retail_aft_vat" },
+
+        ]
+    } );
+} ); 
 
 $(function() {
     $('#promotion').modal('show');
