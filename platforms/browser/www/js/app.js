cf75506f7a01b28ac41893969623005322c0caa1
Pushpan Kumar G C <pushpan@Pushpans-MacBook-Pro.local>
2020-09-08 11:35:33 +0545

    network connection fix

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,78 +1,20 @@
-  
-
 $(document).ready(function() {
-    $('#old-rate-lists').DataTable( {  
-        "ordering": true,     
-        "lengthChange": false,          
-        "pageLength": 50,
-        "ajax": "data.json"
-    } );
+    document.addEventListener("offline", onOffline, false);
+    document.addEventListener("online", onOnline, false);
 
-    function onlineAjaxCall() {
-        $('#rate-lists').DataTable( {  
-            "ordering": true,
-            "lengthChange": false,           
-            "pageLength": 50,
-            "ajax": "https://liquor.pushpan.com.np/api/items/?format=datatables",
-        "columns": [
-                { "data": "title" },
-                { "data": "category" },
-                { "data": "vol" },
-                { "data": "mrp" },
-                { "data": "ws_aft_vat" },
-                { "data": "retail_bf_vat" },
-            { "data": "retail_aft_vat" },
-    
-            ]
-        } );
-        
-    }
-    onlineAjaxCall();
-} );  
 
-document.addEventListener("offline", onOffline, false);
-document.addEventListener("online", onOnline, false);
-
-// function onOnline() {
-//     $(document).ready(function() {
-//         $('#rate-lists').DataTable( {  
-//             "ordering": true,
-//             "lengthChange": false,           
-//             "pageLength": 50,
-//             "ajax": "https://liquor.pushpan.com.np/api/items/?format=datatables",
-//         "columns": [
-//                 { "data": "title" },
-//                 { "data": "category" },
-//                 { "data": "vol" },
-//                 { "data": "mrp" },
-//                 { "data": "ws_aft_vat" },
-//                 { "data": "retail_bf_vat" },
-//             { "data": "retail_aft_vat" },
-    
-//             ]
-//         } );
-//     } );  
-// }
-
-function onOffline() {
-    $(document).ready(function() {
-        $('#rate-lists').DataTable( {  
-            "ordering": true,     
-            "lengthChange": false,          
-            "pageLength": 50,
-            "ajax": "db.json"
-        } );
+    function onOffline() {
         $( "#network" ).removeClass( "d-none" );
-    });  
-}
-function onOnline() {
-    $(document).ready(function() {
-        $( "#network" ).addClass( "d-none" );
-    });  
-    onlineAjaxCall();
-    location.reload();
-}
 
-$(function() {
-    $('#promotion').modal('show');
-});
+    } 
+    function onOnline() {
+        $( "#network-online" ).removeClass( "d-none" );
+        $( "#network-offline" ).addClass( "d-none" );
+
+    }         
+    } );  
+
+
+
+
+   
