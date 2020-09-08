577b2511b743bd389711e95829836c7bbc0b5847
Pushpan Kumar G C <pushpan@Pushpans-MacBook-Pro.local>
2020-09-08 08:51:48 +0545

    network status

--- www/js/app.js
+++ www/js/app.js
@@ -26,6 +26,47 @@ $(document).ready(function() {
     } );
 } );  
 
+document.addEventListener("offline", onOffline, false);
+document.addEventListener("online", onOnline, false);
+
+// function onOnline() {
+//     $(document).ready(function() {
+//         $('#rate-lists').DataTable( {  
+//             "ordering": true,
+//             "lengthChange": false,           
+//             "pageLength": 50,
+//             "ajax": "https://liquor.pushpan.com.np/api/items/?format=datatables",
+//         "columns": [
+//                 { "data": "title" },
+//                 { "data": "category" },
+//                 { "data": "vol" },
+//                 { "data": "mrp" },
+//                 { "data": "ws_aft_vat" },
+//                 { "data": "retail_bf_vat" },
+//             { "data": "retail_aft_vat" },
+    
+//             ]
+//         } );
+//     } );  
+// }
+
+function onOffline() {
+    $(document).ready(function() {
+        $('#rate-lists').DataTable( {  
+            "ordering": true,     
+            "lengthChange": false,          
+            "pageLength": 50,
+            "ajax": "db.json"
+        } );
+        $( "#network" ).removeClass( "d-none" );
+    });  
+}
+function onOnline() {
+    $(document).ready(function() {
+        $( "#network" ).addClass( "d-none" );
+    });  
+}
+
 $(function() {
     $('#promotion').modal('show');
 });
